4b9739b69011bb1f4a848a027a5ba51597597efa
Jonathan Strickland <djanatyn@gmail.com>
2011-07-20 19:16:15 -0400

    actually generates webpages for real this time

--- bb.pl
+++ bb.pl
@@ -1,38 +1,57 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 use strict;
+use warnings;
+use v5.10;
 use Template;
+use IO::File;
+use autodie;
 
 my $tt = Template->new(
 {
     INCLUDE_PATH => './templates',
 });
 
-sub returnHash {
-    my ($self) = @_;
-    my %self;
+# sub parseFile($filename)
+# returns a list of title, date, and text
+sub parseFile {
+    my ($filename) = @_;
+    my %data;
 
-    open(TEXT, "$self") or die "can't open $self";
-    ($self{title}, $self{date}, my @rest) = <TEXT>;
-    close TEXT;
+    my $fh = IO::File->new($filename, 'r');
+    $data{'title'} = <$fh>;
+    $data{'date'} = <$fh>;
+    $data{'text'} = join('', <$fh>);
 
-    $self{text} = join('', @rest);
-
-    return %self;
+    return $data{'title'}, $data{'date'}, $data{'text'};
 }
 
 my @filenames = glob './input/*';
-my %files;
-
-for my $file (@filenames) { $files{%$file} = returnHash($file); }
+#my %files = map { $_ => parseFile($_) } @filenames;
 
 my $indexPosts;
+my $links;
 for my $file (@filenames[0..2]) {
-    $indexPosts .= "<h2>$files{$file}{title} - $files{$file}{date}</h2><hr />$files{$file}{text}<br />\n";
+    my ($title, $date, $text) = parseFile($file);
+    my $name = $file;
+    $name =~ s/.+input\///;
+    $indexPosts .= "<h2>$title - $date</h2><hr />$text<br />\n";
+    $links .= "<li><a href='$name.html'>$name</a></li>";
 }
 
+for my $file (@filenames) {
+    my ($title, $date, $text) = parseFile($file);
+    my $localvars = {
+	content => "<h2>$title - $date</h2><hr />$text<br />\n",
+	links => $links,
+    };
+    $file =~ s/.+input\///;
+    $tt->process('index.tt', $localvars, "./output/$file.html");
+}
+	
 my $vars = {
-    content => "$indexPosts",
-    links => "hello world",
+    content => $indexPosts,
+    links => $links,
 };
 
 $tt->process('index.tt', $vars, './output/index.html');
+
